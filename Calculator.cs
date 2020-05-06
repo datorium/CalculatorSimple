bca1c0e6bd4bf18c17eb0960dc5276ff6e0b941b
Datorium <hello@datorium.lv>
2020-05-06 18:16:40 +0300

    version 18:16

--- Calculator.cs
+++ Calculator.cs
@@ -21,6 +21,7 @@ namespace CalculatorSimple
         private void InitializeCalculator()
         {
             this.BackColor = Color.Purple;
+            Display.Font = new Font("Roboto", 22f);
 
             string buttonName = null;
             Button button = null;
@@ -29,8 +30,15 @@ namespace CalculatorSimple
                 buttonName = "button" + i;
                 button = (Button)this.Controls[buttonName];
                 button.Text = i.ToString();
+                button.BackColor = Color.Pink;
+                button.Font = new Font("Roboto", 22f);
+             }
+        }
 
-            }
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            Display.Text += button.Text;
         }
     }
 }
