66a36ce1f9f284cbf818b4404b46194850730dd7
Datorium <hello@datorium.lv>
2020-05-06 17:26:59 +0300

    version 17:26

--- Calculator.cs
+++ Calculator.cs
@@ -10,11 +10,17 @@ using System.Windows.Forms;
 
 namespace CalculatorSimple
 {
-    public partial class Form1 : Form
+    public partial class Calculator : Form
     {
-        public Form1()
+        public Calculator()
         {
             InitializeComponent();
+            InitializeCalculator();
+        }
+
+        private void InitializeCalculator()
+        {
+            this.BackColor = Color.Purple;
         }
     }
 }
