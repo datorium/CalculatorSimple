ae4a0f15db348c8636032d58aebefa3b7030e725
Datorium <hello@datorium.lv>
2020-05-09 17:42:46 +0300

    version 17:42

--- Calculator.cs
+++ Calculator.cs
@@ -40,5 +40,34 @@ namespace CalculatorSimple
             Button button = (Button)sender;
             Display.Text += button.Text;
         }
+
+        private void buttonDecimal_Click(object sender, EventArgs e)
+        {            
+            if (!Display.Text.Contains("."))
+            {
+                if(Display.Text == string.Empty)
+                {
+                    Display.Text += "0.";
+                }
+                else
+                {
+                    Display.Text += ".";
+                }     
+            }                 
+        }
+
+        private void buttonBackspace_Click(object sender, EventArgs e)
+        {
+            string s = Display.Text;
+            if (s.Length > 1)
+            {
+                s = s.Substring(0, s.Length - 1);
+            }
+            else
+            {
+                s = string.Empty;
+            }
+            Display.Text = s;
+        }
     }
 }
